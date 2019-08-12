fe257b9185ccf341e91cf97f687424d0f30760b1
ashway <kostya.kzo@gmail.com>
2019-08-12 18:10:13 +0500

    Снес все ресурсы из проекта.. вынес в отдельную папку

--- src/server.js
+++ src/server.js
@@ -3,11 +3,9 @@ const Router = require('koa-router');
 const router = new Router();
 let cors = require('koa2-cors');
 const koaBody = require('koa-body');
-const serve = require('koa-static');
 
 const app = new Koa();
 
-app.use(serve(`${__dirname}/static`));
 app.use(koaBody({ multipart: true }));
 app.use(cors({ origin: '*' }));
 app.use(router.allowedMethods());
