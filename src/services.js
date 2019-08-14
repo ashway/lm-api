382af0bb955ba5bcbc52b8b7d9d243cb5693a438
ashway <kostya.kzo@gmail.com>
2019-08-14 13:09:14 +0500

    save

--- src/services.js
+++ src/services.js
@@ -5,7 +5,19 @@ const dbPath = '../lm-api-data/db.sqlite';
 let router = new Router();
 router.get('/services/list', async function (ctx, next) {
     const db = await sqlite.open(dbPath);
-    let rows = await db.all("SELECT * FROM services ORDER BY rowid");
+    let rows = await db.all("SELECT alias alias, group_concat(model, ',') models FROM services GROUP BY alias");
+    ctx.body = rows;
+    ctx.status = 200;
+    db.close();
+    await next();
+});
+
+
+//SELECT * FROM services s LEFT JOIN carcatalog cc ON cc.model IN (s.models||',') WHERE s.alias='premium'
+
+router.get('/services/list/:alias', async function (ctx, next) {
+    const db = await sqlite.open(dbPath);
+    let rows = await db.all("SELECT * FROM services");
     ctx.body = rows;
     ctx.status = 200;
     db.close();
